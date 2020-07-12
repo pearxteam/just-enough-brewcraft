b97ce9fc84a5959437634c721508441af360b6be
mrAppleXZ <mr.applexz@gmail.com>
2020-07-12 16:13:32 +0500

    Add copyright notices

--- src/main/java/net/pearx/jebc/Jebc.java
+++ src/main/java/net/pearx/jebc/Jebc.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Just Enough BrewCraft.
+ *
+ * Just Enough BrewCraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Just Enough BrewCraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Just Enough BrewCraft.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.pearx.jebc;
 
 import net.minecraftforge.fml.common.Mod;
